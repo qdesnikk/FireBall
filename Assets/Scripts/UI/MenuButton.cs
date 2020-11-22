fef30ad5f55a3673423e093fa1e45c2f0abc83b0
qdesnik <pivovarov.vasiliy.np@gmail.com>
2020-11-22 23:12:10 +0300

    add button menu, add menu actions

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class MenuButton : MonoBehaviour
+{
+    private Button _menuButton;
+
+    private void Awake()
+    {
+        _menuButton = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _menuButton.onClick.AddListener(ExitToMenu);
+    }
+
+    private void OnDisable()
+    {
+        _menuButton.onClick.RemoveListener(ExitToMenu);
+    }
+
+    private void ExitToMenu()
+    {
+        SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+        Time.timeScale = 0;
+    }
+}
